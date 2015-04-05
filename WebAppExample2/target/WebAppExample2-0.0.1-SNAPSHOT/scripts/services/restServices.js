7ecf94d52ba584f89f9ae11b570730b4baf82afd
Maciej <maciejax@gmail.com>
2015-04-06 00:17:31 +0200

    remove target dir from git

--- WebAppExample2/target/WebAppExample2-0.0.1-SNAPSHOT/scripts/services/restServices.js
+++
@@ -1,5 +0,0 @@
-var restServices = angular.module('restServices', ['ngResource']);
-
-restServices.factory('TestTextResource', function($resource){
-	return $resource('/WebAppExample2/rest/firstservice', {});
-});
